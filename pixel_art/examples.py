1e98930f3f8676873bf8e9bddd88b8bc40671948
SinaDavari <126830506+SinaDavari@users.noreply.github.com>
2023-08-22 10:08:02 -0400

    Update examples.py

--- pixel_art/examples.py
+++ pixel_art/examples.py
@@ -1,3 +1,15 @@
+import pixels
 
+def colors():
+  print(f"{pixels.BLACK}{pixels.Back.RESET} - 'black'")
+  print(f"{pixels.RED}{pixels.Back.RESET} - 'red'")
+  print(f"{pixels.GREEN}{pixels.Back.RESET} - 'green'")
+  print(f"{pixels.YELLOW}{pixels.Back.RESET} - 'yellow'")
+  print(f"{pixels.BLUE}{pixels.Back.RESET} - 'blue'")
+  print(f"{pixels.MAGENTA}{pixels.Back.RESET} - 'magenta'")
+  print(f"{pixels.CYAN}{pixels.Back.RESET} - 'cyan'")
+  print(f"{pixels.WHITE}{pixels.Back.RESET} - 'white'")
 
 # test your code here
+if __name__ == '__main__':
+  colors()
